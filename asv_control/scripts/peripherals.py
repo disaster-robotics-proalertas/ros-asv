4f60cf607eec349733c1c498ac57517b783c95e6
rgmaidana <renanmaidana@gmail.com>
2020-02-07 10:55:50 -0300

    Changed topics to reference dynamic namespace

--- asv_control/scripts/peripherals.py
+++ asv_control/scripts/peripherals.py
@@ -10,7 +10,7 @@ class Controller:
         # Get vehicle name
         self.vehicle_name = socket.gethostname()
         # Get dictionary of peripherals
-        self.peripherals = rospy.get_param("asv_description/%s/peripherals" % self.vehicle_name)
+        self.peripherals = rospy.get_param('asv_description/peripherals')
 
         # Set handlers for communication
         for p in self.peripherals:
@@ -23,7 +23,7 @@ class Controller:
                 self.peripherals[p]['_handle'] = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
         # Subscribers
-        self.status_sub = rospy.Subscriber('/%s/vehicle/state' % self.vehicle_name, VehicleState, callback=self.status_callback)
+        self.status_sub = rospy.Subscriber('vehicle/state', VehicleState, callback=self.status_callback)
 
         # Rate
         self.rate = rospy.Rate(2)   # 2 Hz
@@ -37,7 +37,7 @@ class Controller:
     def run(self):
         # Handle peripherals according to type
         for p in self.peripherals:
-            if self.peripherals[p]['type'] == "status_light":
+            if self.peripherals[p]['type'] == 'status_light':
                 try:
                     self.peripherals[p]['_handle'].write("%d\n" % self.vehicle_state.id)
                 except serial.SerialTimeoutException:
@@ -47,7 +47,7 @@ class Controller:
 
 
 if __name__ == "__main__":
-    rospy.init_node('asv_control_peripherals', anonymous=True)
+    rospy.init_node('control_peripherals', anonymous=True)
     peripherals_control = Controller()
     while not rospy.is_shutdown():
         peripherals_control.run()
